b95b4ff585baf88e7a1df207151e36d952b8a601
Матвей Лишай <neamico2022@gmail.com>
2023-01-22 18:55:16 +0400

    test pr

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'NEAmico';
+const name = 'NEAmico 2022';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({children, home}) {
